07948b2f6a42bb79441b4d417944cda5fe9fefea
mridentity <mridentity@c3w.com>
2016-05-17 18:07:45 -0400

    Added resource, plugin version and provider class.

--- KeeToReady/KeeToReadyExt.cs
+++ KeeToReady/KeeToReadyExt.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
 
 using KeePass.Plugins;
+using KeePass.DataExchange;
+using System.Drawing;
 
 namespace KeeToReady
 {
     public sealed class KeeToReadyExt : Plugin
     {
         private IPluginHost m_host = null;
+        private FileFormatProvider provider = null;
+        public override Image SmallIcon
+        {
+            get { return Properties.Resources.B16x16_KeePassPlus; }
+        }
+
+        public override string UpdateUrl
+        {
+            get { return "https://github.com/mridentity/KeeToReady/tree/master/KeeToReady/plugin.version"; }
+        }
 
         public override bool Initialize(IPluginHost host)
-        {
-            m_host = host;
+        { 
+            Contract.Requires(host != null);
+
+            this.m_host = host;
+
+            provider = new Rsrz();
+
+            host.FileFormatPool.Add(provider);
+
             return true;
         }
 
         public override void Terminate()
         {
+            if (m_host != null)
+            {
+                m_host.FileFormatPool.Remove(provider);
+            }
         }
     }
 }
---
+++ KeeToReady/Rsrz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeePass.DataExchange;
+
+namespace KeeToReady
+{
+    internal sealed class Rsrz : FileFormatProvider
+    {
+        public override bool SupportsImport { get { return false; } }
+        public override bool SupportsExport { get { return true; } }
+        public override string FormatName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
