9dde6ec0b1e29fd3ada9aa9163ac4f65e5bc4c52
mridentity <mridentity@c3w.com>
2016-05-22 06:18:19 -0400

    Remove code that's no longer needed.

--- KeeToReady/RsoField.cs
+++ KeeToReady/RsoField.cs
@@ -29,6 +29,5 @@ namespace KeeToReady
         public double? dateValue;      //This is timeIntervalSince1970 of NSTimeInterval type on iOS. Refer to this link on how to calculate the dateValue of a given .NET DateTime object http://stackoverflow.com/questions/3354893/how-can-i-convert-a-datetime-to-the-number-of-seconds-since-1970
         public decimal? numberValue;
         public string stringValue;
-        public double lastUpdated; //  This is timeIntervalSinceReferenceDate of NSTimeInterval type on iOS.
     }
 }
--- KeeToReady/RsrzFile.cs
+++ KeeToReady/RsrzFile.cs
@@ -37,121 +37,14 @@ namespace KeeToReady
         // the export function is not frequently triggered by the end user anyway.    
         private const long kKeyDerivationRoundForExport = 1000000;
 
-        private const string ElemDocNode = "KeePassFile";
-        private const string ElemMeta = "Meta";
-        private const string ElemRoot = "Root";
-        private const string ElemGroup = "Group";
-        private const string ElemEntry = "Entry";
-
-        private const string ElemGenerator = "Generator";
-        private const string ElemHeaderHash = "HeaderHash";
-        private const string ElemDbName = "DatabaseName";
-        private const string ElemDbNameChanged = "DatabaseNameChanged";
-        private const string ElemDbDesc = "DatabaseDescription";
-        private const string ElemDbDescChanged = "DatabaseDescriptionChanged";
-        private const string ElemDbDefaultUser = "DefaultUserName";
-        private const string ElemDbDefaultUserChanged = "DefaultUserNameChanged";
-        private const string ElemDbMntncHistoryDays = "MaintenanceHistoryDays";
-        private const string ElemDbColor = "Color";
-        private const string ElemDbKeyChanged = "MasterKeyChanged";
-        private const string ElemDbKeyChangeRec = "MasterKeyChangeRec";
-        private const string ElemDbKeyChangeForce = "MasterKeyChangeForce";
-        private const string ElemRecycleBinEnabled = "RecycleBinEnabled";
-        private const string ElemRecycleBinUuid = "RecycleBinUUID";
-        private const string ElemRecycleBinChanged = "RecycleBinChanged";
-        private const string ElemEntryTemplatesGroup = "EntryTemplatesGroup";
-        private const string ElemEntryTemplatesGroupChanged = "EntryTemplatesGroupChanged";
-        private const string ElemHistoryMaxItems = "HistoryMaxItems";
-        private const string ElemHistoryMaxSize = "HistoryMaxSize";
-        private const string ElemLastSelectedGroup = "LastSelectedGroup";
-        private const string ElemLastTopVisibleGroup = "LastTopVisibleGroup";
-
-        private const string ElemMemoryProt = "MemoryProtection";
-        private const string ElemProtTitle = "ProtectTitle";
-        private const string ElemProtUserName = "ProtectUserName";
-        private const string ElemProtPassword = "ProtectPassword";
-        private const string ElemProtUrl = "ProtectURL";
-        private const string ElemProtNotes = "ProtectNotes";
-        // private const string ElemProtAutoHide = "AutoEnableVisualHiding";
-
-        private const string ElemCustomIcons = "CustomIcons";
-        private const string ElemCustomIconItem = "Icon";
-        private const string ElemCustomIconItemID = "UUID";
-        private const string ElemCustomIconItemData = "Data";
-
-        private const string ElemAutoType = "AutoType";
-        private const string ElemHistory = "History";
-
-        private const string ElemName = "Name";
-        private const string ElemNotes = "Notes";
-        private const string ElemUuid = "UUID";
-        private const string ElemIcon = "IconID";
-        private const string ElemCustomIconID = "CustomIconUUID";
-        private const string ElemFgColor = "ForegroundColor";
-        private const string ElemBgColor = "BackgroundColor";
-        private const string ElemOverrideUrl = "OverrideURL";
-        private const string ElemTimes = "Times";
-        private const string ElemTags = "Tags";
-
-        private const string ElemCreationTime = "CreationTime";
-        private const string ElemLastModTime = "LastModificationTime";
-        private const string ElemLastAccessTime = "LastAccessTime";
-        private const string ElemExpiryTime = "ExpiryTime";
-        private const string ElemExpires = "Expires";
-        private const string ElemUsageCount = "UsageCount";
-        private const string ElemLocationChanged = "LocationChanged";
-
-        private const string ElemGroupDefaultAutoTypeSeq = "DefaultAutoTypeSequence";
-        private const string ElemEnableAutoType = "EnableAutoType";
-        private const string ElemEnableSearching = "EnableSearching";
-
-        private const string ElemString = "String";
-        private const string ElemBinary = "Binary";
-        private const string ElemKey = "Key";
-        private const string ElemValue = "Value";
-
-        private const string ElemAutoTypeEnabled = "Enabled";
-        private const string ElemAutoTypeObfuscation = "DataTransferObfuscation";
-        private const string ElemAutoTypeDefaultSeq = "DefaultSequence";
-        private const string ElemAutoTypeItem = "Association";
-        private const string ElemWindow = "Window";
-        private const string ElemKeystrokeSequence = "KeystrokeSequence";
-
-        private const string ElemBinaries = "Binaries";
-
-        private const string AttrId = "ID";
-        private const string AttrRef = "Ref";
-        private const string AttrProtected = "Protected";
-        private const string AttrProtectedInMemPlainXml = "ProtectInMemory";
-        private const string AttrCompressed = "Compressed";
-
-        private const string ElemIsExpanded = "IsExpanded";
-        private const string ElemLastTopVisibleEntry = "LastTopVisibleEntry";
-
-        private const string ElemDeletedObjects = "DeletedObjects";
-        private const string ElemDeletedObject = "DeletedObject";
-        private const string ElemDeletionTime = "DeletionTime";
-
-        private const string ValFalse = "False";
-        private const string ValTrue = "True";
-
-        private const string ElemCustomData = "CustomData";
-        private const string ElemStringDictExItem = "Item";
-
         private PwDatabase m_pwDatabase; // Not null, see constructor
 
         private IStatusLogger m_slLogger = null;
 
         private RsrzFormat m_format = RsrzFormat.CompressedJsonWithoutEncryption;
 
-        private XmlWriter m_xmlWriter = null;
         private JsonTextWriter m_jsonWriter = null;
 
-        private static bool m_bLocalizedNames = false;
-
-        private Dictionary<string, ProtectedBinary> m_dictBinPool = new Dictionary<string, ProtectedBinary>();
-
-        private byte[] m_pbHashOfHeader = null;
         private byte[] m_pbHashOfFileOnDisk = null;
 
         /// <summary>
